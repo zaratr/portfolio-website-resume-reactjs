bd8529ace516cc126866700a768e92597c82a254
Raul Zarate <101022036+zaratr@users.noreply.github.com>
2022-12-27 12:31:17 -0800

    added final touches to navbar and begin background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './style/App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Skills from './components/Skills';
 import About from './components/About';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
@@ -12,6 +13,7 @@ function App() {
         <Navbar/>
         <Home/>
         <Projects/>
+        <Skills/>
         <About/>
         <Footer/>
     </>
---
+++ src/components/Skills.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../style/skills.css'
+import {Element} from 'react-scroll'
+class Skills extends React.Component{
+    render() {
+        return(
+            <Element className='skills' id='skills'>
+                Skills
+            </Element>
+        );
+    }
+}
+
+export default Skills;
