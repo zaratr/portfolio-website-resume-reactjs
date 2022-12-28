0c4246c80046320353db3cc88a1d83f5501794e5
Raul Zarate <101022036+zaratr@users.noreply.github.com>
2022-12-28 14:58:00 -0800

    added final background animatino

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
 import '../style/home.css'
 import {Element} from 'react-scroll'
+import proImage from './../img/raultemp.jpg'
 class Home extends React.Component{
     render() {
         return(
             <Element className='home' id='home'>
-                <div className='wave'>
+                <div className='layout'>
+                    <p className='intro-paragraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam architecto cum distinctio
+                        earum et hic minus nihil, numquam officia porro quisquam velit veniam, veritatis voluptate? Corporis
+                        minus recusandae veritatis!
+                    </p>
+
+                    <img className='proImage' src={proImage} alt=""/>
                 </div>
+                <div className='wave'></div>
             </Element>
         );
     }
