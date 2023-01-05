73846bea648d2ba1300ba239c4007f4e604c300e
Raul Zarate <101022036+zaratr@users.noreply.github.com>
2023-01-04 20:23:30 -0800

    added fixes to navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ class Home extends React.Component{
         return(
             <Element className='home' id='home'>
                 <div className='layout'>
-                    <p className='intro-paragraph text-white'>
+                    <p className='intro-paragraph fw-3 fw-light text-white'>
                         Hi!  I'm Raul, a Fullstack Developer with a Mathematics Degree. Projects below demonstrate some of
                         my skills on my journey in becoming a Computer Scientist.
                     </p>
