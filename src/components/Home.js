964781bc10d401b3621668299b70b21fb93a8b8d
Raul Zarate <101022036+zaratr@users.noreply.github.com>
2023-01-04 20:50:37 -0800

    added animation to image

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import '../style/home.css'
 import {Element} from 'react-scroll'
 import proImage from './../img/raultemp.jpg'
 import { Fade } from "react-reveal";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 class Home extends React.Component{
     constructor(props) {
@@ -17,9 +18,9 @@ class Home extends React.Component{
                         Hi!  I'm Raul, a Fullstack Developer with a Mathematics Degree. Projects below demonstrate some of
                         my skills on my journey in becoming a Computer Scientist.
                     </p>
-                    <Fade delay={500}>
 
-                        <img className='proImage avatar-wobble' src={proImage} alt=""/>
+                    <Fade delay={500}>
+                        <Image className='proImage avatar-wobble m-2' src={proImage} alt=""/>
                     </Fade>
                 </div>
                 <div className='wave'></div>
